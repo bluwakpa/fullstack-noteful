55c6cd10584059c134ad15b00e095943a37778b2
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-04-03 19:03:04 -0700

    Calendar CSS

--- src/StudentCalendar.js
+++ src/StudentCalendar.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import Calendar from 'react-calendar';
-import { differenceInCalendarDays } from 'date-fns';
+import { differenceInCalendarDays, addDays } from 'date-fns';
 import data from './data';
 import ApiContext from './ApiContext';
 import 'react-calendar/dist/Calendar.css';
@@ -9,23 +9,30 @@ export default function StudentCalendar({ match }) {
     const [value, onChange] = useState(new Date());
     const context = useContext(ApiContext);
     const student = context.students.find(student => student.id === match.params.id);
-    const datesToAddClassTo = [student.present, data.attendance];
+    // const datesToAddClassTo = [yesterday];
 
     console.log(student)
+
     const updateStudents = (newStudent) => {
         context.setStudents([...context.students, newStudent])
         // props.history.push(`/attendance/${period}`)
     }
 
-    function isSameDay(a, b) {
-        return differenceInCalendarDays(a, b) === 0;
+    function isSameDay(student, match) {
+        return differenceInCalendarDays(student, match) === 0;
     }
 
     function tileClassPresent({ date, view }) {
+        let today = new Date()
+        // let yesterday = addDays(today, -2)
+        // console.log(yesterday, date)
         // Add class to tiles in month view only
         if (view === 'month') {
           // Check if a date React-Calendar wants to check is on the list of dates to add class to
-          if (datesToAddClassTo.find(dDate => isSameDay(dDate, date))) {
+          // add an if else statement
+          if (differenceInCalendarDays(today, date) === 0) {
+              console.log('check', date)
+              
             return 'myClassPresent';
           }
         }
@@ -44,7 +51,7 @@ export default function StudentCalendar({ match }) {
                     <Calendar
                         onChange={onChange}
                         value={value}
-                        tileClassPresent={tileClassPresent}
+                        tileClassName={tileClassPresent}
                     />
                 </div>
             </header>
